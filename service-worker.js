d3870d36f6617b63c13a77f83dda6347ad880da6
strvsn <h.kono@strvsn.net>
2025-06-05 09:09:37 +0900

    PWA対応: manifest.json, service-worker.js追加index.html修正

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'ulsa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/windChart.js',
+  '/serial.js',
+  '/ui.js',
+  '/style.css',
+  '/chart.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
